776ccb5f882e1fac9948e67fcac3d4eac14ad88e
Ishaq Tasleem Mohammad <58343363+ismohammad@users.noreply.github.com>
2019-12-01 19:52:10 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import {routerConfig} from "../router.config";
+import {RouterModule} from "@angular/router";
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseCardsComponent } from './course-cards/course-cards.component';
+import { SideMenuComponent } from './categories-menu/categories-menu.component';
+import { CoursesCategoryComponent } from './course-category/course-category.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CoursesComponent,
+    CourseCardsComponent,
+    SideMenuComponent,
+    CoursesCategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routerConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
